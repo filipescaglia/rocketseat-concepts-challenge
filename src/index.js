1ea8442952816a8203035d6d75007f8cd8e948cf
Filipe Scaglia <filipecscaglia@gmail.com>
2021-11-05 17:45:03 -0300

    Feature: get user todos

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find(user => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  request.user = user;
+  next();
 }
 
 app.post('/users', (request, response) => {
@@ -32,7 +39,8 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
